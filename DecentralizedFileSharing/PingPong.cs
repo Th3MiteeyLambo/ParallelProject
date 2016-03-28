ecd42e3859742f9b7412d21ba3c3bdf43db664b5
Th3MiteeyLambo <th3miteeylambo@hotmail.com>
2016-03-28 14:05:19 -0500

    Started on the code for sending pings and recieving pings.

---
+++ DecentralizedFileSharing/PingPong.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentralizedFileSharing
+{
+    public static class PingPong
+    {
+        private static byte[] ping = Encoding.ASCII.GetBytes("ping");
+        private static byte[] pong = Encoding.ASCII.GetBytes("pong");
+        private static Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static Socket hearSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        public static bool sendPing(int port, string ip)
+        {
+            IPAddress address = IPAddress.Parse(ip);
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+            try
+            {
+                sendSocket.SendTo(ping, endPoint);
+                Console.WriteLine("No Socket Errors");
+                return true;
+            }
+            catch(SocketException se)
+            {
+                Console.WriteLine("Something went wrong with the socket");
+                Console.WriteLine(se.StackTrace);
+                return false;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong with ping");
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+            
+        }
+
+        public static bool sendPong()
+        {
+            return true;
+        }
+
+        //Mostly copied from Stack Overflow, haven't figured out how it works yet.
+        public static bool hearPong(int port)
+        {
+            byte[] data = null;
+
+            IPEndPoint server = new IPEndPoint(IPAddress.Any, port);
+            hearSocket.Bind(server);
+
+            Console.Write("Waiting for client");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            int recv = hearSocket.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("Message received from {0}:", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            return true;
+        }
+    }
+}
--- DecentralizedFileSharing/UserInterface.cs
+++ DecentralizedFileSharing/UserInterface.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DecentralizedFileSharing
 {
-    public partial class Form1 : Form
+    public partial class UserInterface : Form
     {
-        public Form1()
+        public UserInterface()
         {
             InitializeComponent();
         }
