b198cfae9f86b7ba9153ee43da652fc763baa034
kevgreg <kevgreg94@gmail.com>
2016-05-01 15:29:40 -0500

    registry add

---
+++ DecentralizedFileSharing/UpdateRegistry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DecentralizedFileSharing
+{
+    class UpdateRegistry
+    {
+        private static string path = "C:" + Path.DirectorySeparatorChar + "dir" + Path.DirectorySeparatorChar + "registry.csv";
+
+        public static Boolean add(string ip, string port)
+        {
+
+            string add = ip + ": " +port + "," + System.Environment.NewLine;
+
+            File.AppendAllText(path, add);
+
+            return true;
+        }
+
+        public static void delete()
+        {
+
+        }
+    }
+}
